0d2268ddb2c397f3ed607dbdfcdedb925be26c43
Adam Lear <adamlear12@gmail.com>
2024-03-27 15:55:56 -0500

    add max's work to app

--- app.py
+++ app.py
@@ -6,53 +6,61 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index.html')
+
+@app.route("/")
+@app.route("/index.html")
 def index2():
     conn = SQLConnection()
     conn = conn.getConnection()
     cursor = conn.cursor()
 
-    sql_query = """SELECT PotholeID, Latitude, Longitude FROM Potholes.PotholeLocation"""
+    sql_query = (
+        """SELECT PotholeID, Latitude, Longitude FROM Potholes.PotholeLocation"""
+    )
     cursor.execute(sql_query)
     records = cursor.fetchall()
     cursor.close()
-    return render_template('index.html', records = records)
+    return render_template("index.html", records=records)
 
-@app.route('/edit.html')
+
+@app.route("/edit.html")
 def index3():
     conn = SQLConnection()
     conn = conn.getConnection()
     cursor = conn.cursor()
 
-    sql_query = """SELECT PotholeID, Latitude, Longitude FROM Potholes.PotholeLocation"""
+    sql_query = (
+        """SELECT PotholeID, Latitude, Longitude FROM Potholes.PotholeLocation"""
+    )
     cursor.execute(sql_query)
     records = cursor.fetchall()
     cursor.close()
 
-    return render_template('edit.html', records = records)
+    return render_template("edit.html", records=records)
 
 
-@app.route('/edit.html', methods=['POST'])
+@app.route("/edit.html", methods=["POST"])
 def edit_entry():
-    if request.method == 'POST':
-        action = request.form.get('action')
-        
-        if action == 'delete':
-            pothole_id = request.form.get('PotholeID')
+    if request.method == "POST":
+        action = request.form.get("action")
+
+        if action == "delete":
+            pothole_id = request.form.get("PotholeID")
             conn = SQLConnection()
             conn = conn.getConnection()
             cursor = conn.cursor()
-            delete_query = """DELETE FROM Potholes.PotholeLocation WHERE PotholeID = ?"""
+            delete_query = (
+                """DELETE FROM Potholes.PotholeLocation WHERE PotholeID = ?"""
+            )
             cursor.execute(delete_query, (pothole_id,))
             conn.commit()
             cursor.close()
             return "Entry deleted successfully"
-        
-        elif action == 'add':
-            pothole_id = request.form.get('PotholeID')
-            latitude = request.form.get('Latitude')
-            longitude = request.form.get('Longitude')
+
+        elif action == "add":
+            pothole_id = request.form.get("PotholeID")
+            latitude = request.form.get("Latitude")
+            longitude = request.form.get("Longitude")
             conn = SQLConnection()
             conn = conn.getConnection()
             cursor = conn.cursor()
--- azuresqlconnector.py
+++ azuresqlconnector.py
@@ -6,26 +6,33 @@ class SQLConnection(object):
     __instance = None
     __connection = None
 
-
     def __new__(cls):
         if SQLConnection.__instance is None:
-            SQLConnection.__instance = object.__new__(cls)        
-        return SQLConnection.__instance  
-
+            SQLConnection.__instance = object.__new__(cls)
+        return SQLConnection.__instance
 
     def getConnection(self):
+        server = "sqlserverblokhuisryan01.database.windows.net"
+        database = "sqldbblokhuisryan01"
+        username = "sqlserveradmin"
+        password = "Password123"
+        driver = "{ODBC Driver 18 for SQL Server}"
 
-        server = 'sqlserverblokhuisryan01.database.windows.net'
-        database = 'sqldbblokhuisryan01'
-        username = 'sqlserveradmin'
-        password = 'Password123'
-        driver= '{ODBC Driver 18 for SQL Server}'
+        if self.__connection == None:
+            self.__connection = pyodbc.connect(
+                "DRIVER="
+                + driver
+                + ";SERVER=tcp:"
+                + server
+                + ";PORT=1433;DATABASE="
+                + database
+                + ";UID="
+                + username
+                + ";PWD="
+                + password
+            )
 
-
-        if (self.__connection == None):
-            self.__connection = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
-        
         return self.__connection
-    
+
     def removeConnection(self):
         self.__connection = None
